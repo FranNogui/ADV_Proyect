b432324bcf23159870f85c0a0765af715b0907d8
FranNogui <116107405+FranNogui@users.noreply.github.com>
2023-03-24 19:52:27 +0100

    State Machine Commit

---
+++ ADV_Proyect/Assets/StateMachines/ControlBomb.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlBomb : MonoBehaviour
+{
+    Vector3 pos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pos = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Rebirth()
+    {
+        this.transform.position = pos;
+    }
+}
